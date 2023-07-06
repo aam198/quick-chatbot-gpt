47659520d59276ee196923b83a9bf30b26e55760
aam198 <aam198@psu.edu>
2023-07-06 11:51:09 -0400

    Connecting to instance of OpenAi

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const express = require("express");
+require("dotenv").config();
+
+const { Configuration, OpenAIApi } = require("openai")
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration);
+
+
 
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 // POST request endpoint
+app.post("/ask", async (req, res) => {
+  // getting prompt question from request
+  const prompt = req.body.prompt;
+
+  try {
+    if (prompt == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+    // return the result
+    return res.status(200).json({
+      success: true,
+      message: prompt,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}!!`));
 
 
 
-// const { Configuration, OpenAIApi } = require("openai")
-// require('dotenv').config()
 
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY
-// })
 
-// const openai = new OpenAIApi(configuration);
 
-// console.log(process.env.TEST);
-// console.log(openai)
