1e167af188c0f03ece2521e4595e300dd0aadf95
aam198 <aam198@psu.edu>
2023-07-11 14:00:06 -0400

    Debuggin backend side

--- index.js
+++ index.js
@@ -1,69 +1,46 @@
 const express = require('express');
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
-
 const port = process.env.PORT || 3000;
 const app = express();
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
-
 const openai = new OpenAIApi(configuration);
-
-
 const conversationArr = [
   {
     role: 'system',
     content: 'You are useful assistance.' //this is the instruction
   }
 ];
-
 app.get('/api/conversation', (req, res) => {
   res.json(conversationArr);
 });
-
 // POST request endpoint
-app.post('/api/conversation', (req, res) => {
+app.post('/api/conversation', async (req, res) => {
   // getting prompt question from request
-  const { prompt } = req.body;
-
-  const newSpeechBubble = {
-    role: 'user',
-    content: userInput,
-  };
-
-  conversationArr.push(newSpeechBubble);
-  openai.chat
-    .create({
-      messages: conversationArr,
-    })
-    .then((response) => {
-      const chatResponse = response.data.choices[0].message.content;
-
-      const chatBubble = {
-        role: 'assistant',
-        content: chatResponse,
-      };
-
-      conversationArr.push(chatBubble);
-
-      res.json({ response: chatResponse });
-    })
-    .catch((error) => {
-      console.error('OpenAI API error:', error);
-      res.status(500).json({ error: 'An error occurred' });
+  const prompt = req.body.prompt;
+  try {
+    if (prompt == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+   // trigger OpenAI completion
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
     });
+    // retrieve the completion text from response
+    const completion = response.data.choices[0].text;
+    // returned in successful response of /ask request
+    return res.status(200).json({
+      success: true,
+      message: completion,
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
 });
-
 app.listen(port, () => console.log(`Server is running on port ${port}!!`));
-
-
-
-
-
-
