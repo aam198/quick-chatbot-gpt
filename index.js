4fdebf28b87eaa74ab19ab44c88b1d68a132d375
aam198 <aam198@psu.edu>
2023-07-12 10:58:00 -0400

    Switched AI Model to gpt, working on storing conversations

--- index.js
+++ index.js
@@ -31,18 +31,25 @@ app.get('/api/conversation', (req, res) => {
 app.post('/api/conversation', async (req, res) => {
   // getting prompt question from request
   const prompt = req.body.prompt;
+  console.log('this is the req.body.promp', req.body.prompt)
+  conversationArr.push({
+    role: 'user',
+    content: prompt
+  })
+  console.log(conversationArr);
+
   try {
     if (prompt == null) {
       throw new Error("Uh oh, no prompt was provided");
     }
    // trigger OpenAI completion
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt,
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: conversationArr,
     });
     // retrieve the completion text from response
     console.log(response.data);
-    const completion = response.data.choices[0].text;
+    const completion = response.data.choices[0].message.content;
     // returned in successful response of /ask request
     return res.status(200).json({
       success: true,
